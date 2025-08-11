da4caac35b8fed466bbae6a9e0844bf25c752876
tyler <ttylerzh@gmail.com>
2025-08-11 16:37:22 +0800

    update

--- test.py
+++
@@ -1,9 +0,0 @@
-import arxiv
-
-result = arxiv.Search(
-    query = "",
-    max_results = 100,
-    sort_by = arxiv.SortCriterion.SubmittedDate
-)
-for result in result.results():
-    print(result.entry_id, '->', result.title)
